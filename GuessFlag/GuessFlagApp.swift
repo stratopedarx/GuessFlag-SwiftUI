2033ba8940444befaeaf6309e80f8baca365b952
Sergey Lobanov <sergey.lobanov@orioninc.com>
2021-10-20 15:57:52 +0300

    Initial Commit

---
+++ GuessFlag/GuessFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessFlagApp.swift
+//  GuessFlag
+//
+//  Created by Sergey Lobanov on 20.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
